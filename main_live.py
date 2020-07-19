c7cbd26e3b7657b60f15c2b3737d7794bbdce2ba
Theodoros Giannakopoulos <tyiannak@gmail.com>
2020-07-19 19:48:03 +0300

    add wav save

--- main_live.py
+++ main_live.py
@@ -4,9 +4,37 @@
 import numpy as np
 import pyaudio
 import struct
+import sys
+import datetime
+import signal
+import time
+import scipy.io.wavfile as wavfile
 
-buff_size = 0.01          # window size in seconds
+
+global fs
+global all_data
+global outstr
+
 fs = 96000                # sampling frequency in Hz
+
+
+def signal_handler(signal, frame):
+    """
+    This function is called when Ctr + C is pressed and is used to output the
+    final buffer into a WAV file
+    """
+    # write final buffer to wav file
+    if len(all_data) > 1:
+        wavfile.write(outstr + ".wav", fs, np.int16(all_data))
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+buff_size = 0.01          # window size in seconds
+
+all_data = []
+time_start = time.time()
+outstr = datetime.datetime.now().strftime("%Y_%m_%d_%I:%M%p")
 
 # initialize soundcard for recording:
 pa = pyaudio.PyAudio()
@@ -18,8 +46,12 @@ while 1:  # for each recorded window (until ctr+c) is pressed
     block = stream.read(int(fs * buff_size))
     format = "%dh" % (len(block) / 2)
     shorts = struct.unpack(format, block)
+    shorts_list = list(shorts)
 
     # then normalize and convert to numpy array:
-    x = np.double(list(shorts)) / (2**15)
+    x = np.double(shorts_list) / (2**15)
     seg_len = len(x)
     print(len(x))
+
+    all_data += shorts_list
+
