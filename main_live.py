b634e980782debd722b871a8d32e52461956725e
Theodoros Giannakopoulos <tyiannak@gmail.com>
2020-07-20 23:28:40 +0300

    incremental threshold calculation in get_syllables()

--- main_live.py
+++ main_live.py
@@ -33,6 +33,11 @@ signal.signal(signal.SIGINT, signal_handler)
 buff_size = 0.01             # recording buffer size in seconds
 mid_buffer_size = 0.5        # processing buffer size in seconds
 st_win = 0.002               # short-term window size in seconds
+F1 = 20000                   # lower spectral energy sequence
+F2 = 40000                   # higher spectral energy sequence
+MIN_VOC_DUR = 0.005
+thres = 1.3
+
 
 all_data = []
 mid_buffer = []
@@ -43,6 +48,8 @@ outstr = datetime.datetime.now().strftime("%Y_%m_%d_%I:%M%p")
 pa = pyaudio.PyAudio()
 stream = pa.open(format=pyaudio.paInt16, channels=1, rate=fs,
                  input=True, frames_per_buffer=int(fs * buff_size))
+
+means = []
 
 while 1:  # for each recorded window (until ctr+c) is pressed
     # get current block and convert to list of short ints,
@@ -59,10 +66,34 @@ while 1:  # for each recorded window (until ctr+c) is pressed
 
     mid_buffer += shorts_list
     if len(mid_buffer) >= int(mid_buffer_size * fs):
+        # get spectrogram:
         spectrogram, sp_time, sp_freq, fs = ap.get_spectrogram_buffer(mid_buffer,
                                                                       fs,
                                                                       st_win,
                                                                       st_win)
+
+        # get spectral sequences:
+        f_low = F1 if F1 < fs / 2.0 else fs / 2.0
+        f_high = F2 if F2 < fs / 2.0 else fs / 2.0
+
+        # define feature sequence for vocalization detection
+        f1 = np.argmin(np.abs(sp_freq - f_low))
+        f2 = np.argmin(np.abs(sp_freq - f_high))
+
+        spectral_energy_1 = spectrogram.sum(axis=1)
+        spectral_energy_2 = spectrogram[:, f1:f2].sum(axis=1)
+
+        print(spectral_energy_1.shape)
+        print(spectral_energy_2.shape)
+        means.append(spectral_energy_2.mean())
+        seg_limits, thres_sm, _ = ap.get_syllables(spectral_energy_2,
+                                                   spectral_energy_1,
+                                                   st_win,
+                                                   threshold_per=thres * 100,
+                                                   min_duration=MIN_VOC_DUR,
+                                                   threshold_buf = means)
+        print(len(seg_limits))
+
         mid_buffer = []
 
 
