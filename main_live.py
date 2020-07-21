598bcf2101c3f9e2c4efb67bdf357701658a4b5d
Theodoros Giannakopoulos <tyiannak@gmail.com>
2020-07-21 18:12:15 +0300

    offline debuging mode added

--- main_live.py
+++ main_live.py
@@ -10,13 +10,17 @@ import signal
 import time
 import scipy.io.wavfile as wavfile
 import audio_process as ap
+from pyAudioAnalysis import audioBasicIO as io
 
 global fs
 global all_data
 global outstr
 
 fs = 96000                # sampling frequency in Hz
-
+global wav_signal
+wav_signal = None
+# comment this line if you want realtime mode
+fs, wav_signal = io.read_audio_file("data/B148_test_small.wav")
 
 def signal_handler(signal, frame):
     """
@@ -31,10 +35,10 @@ def signal_handler(signal, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 buff_size = 0.01             # recording buffer size in seconds
-mid_buffer_size = 0.5        # processing buffer size in seconds
+mid_buffer_size = 1.0        # processing buffer size in seconds
 st_win = 0.002               # short-term window size in seconds
-F1 = 20000                   # lower spectral energy sequence
-F2 = 40000                   # higher spectral energy sequence
+F1 = 30000                   # lower spectral energy sequence
+F2 = 110000                  # higher spectral energy sequence
 MIN_VOC_DUR = 0.005
 thres = 1.3
 
@@ -44,20 +48,28 @@ mid_buffer = []
 time_start = time.time()
 outstr = datetime.datetime.now().strftime("%Y_%m_%d_%I:%M%p")
 
-# initialize soundcard for recording:
-pa = pyaudio.PyAudio()
-stream = pa.open(format=pyaudio.paInt16, channels=1, rate=fs,
-                 input=True, frames_per_buffer=int(fs * buff_size))
+if wav_signal is None:
+    # initialize soundcard for recording:
+    pa = pyaudio.PyAudio()
+    stream = pa.open(format=pyaudio.paInt16, channels=1, rate=fs,
+                     input=True, frames_per_buffer=int(fs * buff_size))
 
 means = []
+count_bufs = 0
+count_mid_bufs = 0
 
 while 1:  # for each recorded window (until ctr+c) is pressed
-    # get current block and convert to list of short ints,
-    block = stream.read(int(fs * buff_size))
-    format = "%dh" % (len(block) / 2)
-    shorts = struct.unpack(format, block)
-    shorts_list = list(shorts)
-
+    if wav_signal is None:
+        # get current block and convert to list of short ints,
+        block = stream.read(int(fs * buff_size))
+        format = "%dh" % (len(block) / 2)
+        shorts = struct.unpack(format, block)
+        shorts_list = list(shorts)
+    else:
+        shorts_list = wav_signal[int(count_bufs * buff_size * fs):
+                             int((count_bufs + 1) * buff_size * fs)].tolist()
+        if int((count_bufs + 1) * buff_size * fs) > len(wav_signal):
+            break
     # then normalize and convert to numpy array:
     x = np.double(shorts_list) / (2**15)
     seg_len = len(x)
@@ -83,8 +95,6 @@ while 1:  # for each recorded window (until ctr+c) is pressed
         spectral_energy_1 = spectrogram.sum(axis=1)
         spectral_energy_2 = spectrogram[:, f1:f2].sum(axis=1)
 
-        print(spectral_energy_1.shape)
-        print(spectral_energy_2.shape)
         means.append(spectral_energy_2.mean())
         seg_limits, thres_sm, _ = ap.get_syllables(spectral_energy_2,
                                                    spectral_energy_1,
@@ -92,8 +102,13 @@ while 1:  # for each recorded window (until ctr+c) is pressed
                                                    threshold_per=thres * 100,
                                                    min_duration=MIN_VOC_DUR,
                                                    threshold_buf = means)
-        print(len(seg_limits))
+        for s in seg_limits:
+            print(count_mid_bufs * mid_buffer_size + s[0],
+                  count_mid_bufs * mid_buffer_size + s[1])
 
         mid_buffer = []
+        count_mid_bufs += 1
+
+    count_bufs += 1
 
 
