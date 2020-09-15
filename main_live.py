c71bde42fc3590b7c05f08611fdd1ad90b2a58bf
tyiannak <tyiannak@gmail.com>
2020-09-15 17:09:38 +0300

    online vs offline automatic comparison started

--- main_live.py
+++ main_live.py
@@ -16,9 +16,16 @@ from pyAudioAnalysis import audioBasicIO as io
 global fs
 global all_data
 global outstr
-
-fs = 44100                # sampling frequency in Hz
 global wav_signal
+
+buff_size = 0.01  # recording buffer size in seconds
+mid_buffer_size = 1.0  # processing buffer size in seconds
+st_win = 0.002  # short-term window size in seconds
+F1 = 30000  # lower spectral energy sequence
+F2 = 110000  # higher spectral energy sequence
+MIN_VOC_DUR = 0.005
+thres = 1.3
+
 wav_signal = None
 
 def signal_handler(signal, frame):
@@ -27,8 +34,35 @@ def signal_handler(signal, frame):
     final buffer into a WAV file
     """
     # write final buffer to wav file
+    global fs
     if len(all_data) > 1:
         wavfile.write(outstr + ".wav", fs, np.int16(all_data))
+
+
+    spectrogram, sp_time, sp_freq, fs = ap.get_spectrogram(outstr+".wav",
+                                                           st_win, st_win)
+    print(spectrogram)
+
+    f_low = F1 if F1 < fs / 2.0 else fs / 2.0
+    f_high = F2 if F2 < fs / 2.0 else fs / 2.0
+
+    # define feature sequence for vocalization detection
+    f1 = np.argmin(np.abs(sp_freq - f_low))
+    f2 = np.argmin(np.abs(sp_freq - f_high))
+
+    spectral_energy_1 = spectrogram.sum(axis=1)
+    spectral_energy_2 = spectrogram[:, f1:f2].sum(axis=1)
+
+    seg_limits, thres_sm, _ = ap.get_syllables(spectral_energy_2,
+                                               spectral_energy_1,
+                                               st_win,
+                                               threshold_per=thres * 100,
+                                               min_duration=MIN_VOC_DUR)
+    for s in seg_limits:
+        with open("debug_offline.csv", "a") as fp:
+            fp.write(f'{count_mid_bufs * mid_buffer_size + s[0]},'
+                     f'{count_mid_bufs * mid_buffer_size + s[1]}\n')
+
     sys.exit(0)
 
 def parse_arguments():
@@ -42,18 +76,13 @@ if __name__ == "__main__":
     args = parse_arguments()
     input_file = args.input_file
     print(input_file)
+    global fs
     if input_file:
         fs, wav_signal = io.read_audio_file(input_file)
+    else:
+        fs = 44100
 
     signal.signal(signal.SIGINT, signal_handler)
-    buff_size = 0.01             # recording buffer size in seconds
-    mid_buffer_size = 1.0        # processing buffer size in seconds
-    st_win = 0.002               # short-term window size in seconds
-    F1 = 30000                   # lower spectral energy sequence
-    F2 = 110000                  # higher spectral energy sequence
-    MIN_VOC_DUR = 0.005
-    thres = 1.3
-
 
     all_data = []
     mid_buffer = []
@@ -68,6 +97,10 @@ if __name__ == "__main__":
     means = []
     count_bufs = 0
     count_mid_bufs = 0
+
+    # get spectral sequences:
+    f_low = F1 if F1 < fs / 2.0 else fs / 2.0
+    f_high = F2 if F2 < fs / 2.0 else fs / 2.0
 
     while 1:  # for each recorded window (until ctr+c) is pressed
         if wav_signal is None:
@@ -90,14 +123,11 @@ if __name__ == "__main__":
         mid_buffer += shorts_list
         if len(mid_buffer) >= int(mid_buffer_size * fs):
             # get spectrogram:
-            spectrogram, sp_time, sp_freq, fs = ap.get_spectrogram_buffer(mid_buffer,
+            print(len(mid_buffer))
+            spectrogram, sp_time, sp_freq, _  = ap.get_spectrogram_buffer(mid_buffer,
                                                                           fs,
                                                                           st_win,
                                                                           st_win)
-
-            # get spectral sequences:
-            f_low = F1 if F1 < fs / 2.0 else fs / 2.0
-            f_high = F2 if F2 < fs / 2.0 else fs / 2.0
 
             # define feature sequence for vocalization detection
             f1 = np.argmin(np.abs(sp_freq - f_low))
@@ -114,8 +144,9 @@ if __name__ == "__main__":
                                                        min_duration=MIN_VOC_DUR,
                                                        threshold_buf = means)
             for s in seg_limits:
-                print(count_mid_bufs * mid_buffer_size + s[0],
-                      count_mid_bufs * mid_buffer_size + s[1])
+                with open("debug_realtime.csv", "a") as fp:
+                    fp.write(f'{count_mid_bufs * mid_buffer_size + s[0]},'
+                             f'{count_mid_bufs * mid_buffer_size + s[1]}\n')
 
             mid_buffer = []
             count_mid_bufs += 1
