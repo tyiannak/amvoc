692856f183431aadd30d9d51a7e45a61c33eb4a0
vassilina <vassilina.stoumpou@gmail.com>
2021-04-05 13:40:31 +0300

    scripts for syllable comparison

--- run_offline.sh
+++ run_offline.sh
@@ -6,6 +6,6 @@ for rec in "${recordings[@]}"; do
      echo "--------------------------------------------"
      echo "Recording $rec"
      python3 main.py -i data/vocalizations_evaluation/"$rec"/rec_"$rec".wav -c n
-     python3 syllables_comp.py -i data/rec_"$rec".wav -csv1 offline_vocalizations.csv -csv2 data/vocalizations_evaluation/"$rec"/gt_"$rec".csv
+     python3 syllables_comp.py -i data/vocalizations_evaluation/"$rec"/rec_"$rec".wav -csv1 offline_vocalizations.csv -csv2 data/vocalizations_evaluation/"$rec"/gt_"$rec".csv
 done 
 
--- run_online.sh
+++ run_online.sh
@@ -6,6 +6,6 @@ for rec in "${recordings[@]}"; do
      echo "--------------------------------------------"
      echo "Recording $rec"
      python3 main_live.py -i data/vocalizations_evaluation/"$rec"/rec_"$rec".wav
-     python3 syllables_comp.py -i data/rec_"$rec".wav -csv1 realtime_vocalizations.csv -csv2 data/vocalizations_evaluation/"$rec"/gt_"$rec".csv
+     python3 syllables_comp.py -i data/vocalizations_evaluation/"$rec"/rec_"$rec".wav -csv1 realtime_vocalizations.csv -csv2 data/vocalizations_evaluation/"$rec"/gt_"$rec".csv
 done 
 
