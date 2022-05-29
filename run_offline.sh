57ff3d1e9e21d8dd10336dd93b770d18d0d0f46d
Christodoulos Bochalis <bojito90@gmail.com>
2022-05-29 21:11:01 +0300

    Scripts for new folder structure

--- run_offline.sh
+++ run_offline.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+cd src 
+
 recordings=(1 2 3 4 5 6 7 8 9)
 
 for rec in "${recordings[@]}"; do
      echo "--------------------------------------------"
      echo "Recording $rec"
-     python3 main.py -i data/vocalizations_evaluation/"$rec"/rec_"$rec".wav -c n
-     python3 syllables_comp.py -i data/vocalizations_evaluation/"$rec"/rec_"$rec".wav -csv1 offline_vocalizations.csv -csv2 data/vocalizations_evaluation/"$rec"/gt_"$rec".csv
+     python main.py -i ../data/vocalizations_evaluation/"$rec"/rec_"$rec".wav -c n
+     python syllables_comp.py -i ../data/vocalizations_evaluation/"$rec"/rec_"$rec".wav -csv1 app_data/offline_rec_"$rec".csv -csv2 ../data/vocalizations_evaluation/"$rec"/gt_"$rec".csv
 done 
+
+cmd /k
+
+
+
 
